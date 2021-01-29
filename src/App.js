536daf9e7b467a4088b85f43822c265964f0eac5
vivekhere <vivekjai10@gmail.com>
2021-01-29 20:55:56 +0530

    link

--- src/App.js
+++ src/App.js
@@ -29,16 +29,4 @@ class App extends Component {
 
 export default App;
 
-// The matching algorithm that the Route component uses checks to see if the current
-// url starts with the path specified if so then the component passed will be rendered.
-// That means if we go to /products/something the component will still be rendered.
-// Since, the path for home is "/" and the path for other components starts with "/"
-// the Home component will also be rendered when we go to other components.
-
-// To solve this probem
-// One solution is to add exact in the Home Route.
-// The other solution is to use the Switch component.
-// On the top import Switch component and wrap all the routes with a switch.
-// The Switch will render the first child that matches the location.
-// With switch we no longer need 'exact'.
-// We should order our routes from the most specific one's to the most generic one's.
+//
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/products">Products</a>
+        <Link to="/products">Products</Link>
       </li>
       <li>
-        <a href="/posts/2018/06">Posts</a>
+        <Link to="/posts/2018/06">Posts</Link>
       </li>
       <li>
-        <a href="/admin">Admin</a>
+        <Link to="/admin">Admin</Link>
       </li>
     </ul>
   );
 };
 
 export default NavBar;
+
+// Even though we have implemented routing, we still have few problems
+// When the App component loads, a file named bundle.js is downloaded which
+// contains all the js code of our component tree.
+// Whenever we click on any of the links in the navigation bar new http requests
+// are made on the server and the bundle.js file is downloaded again.
+// We don't want to download the bundle.js file everytime we click on a link.
+
+// Example Gmail :
+// Gmail is super fast. As you click on an email you quickly see the contents
+// of your email. The entire page is not reloaded.
+// We refer applications build this way as Single Page Applications or SPAs.
+
+// In single page applications when the user navigates from one page to another
+// instead of reloading the entire page with all it's assets, we should only
+// update what we have in the content area.
+
+// So inorder to fix this issue of full reload,
+// on the top import Link from react-router-dom
+// and replace the anchor "a" with "Link" and replace the "href" with "to".
+// Now when we click on the links we are not going to see new requests because
+// the content of the components are in bundle.js. All our components are part
+// of the bundle that are downloaded ahead of time when our applications loads.
+// So there is no need to redownload this component whenever the user navigates.
