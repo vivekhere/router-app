7b899025d94a91fb5cbdac08156697e49b3ff306
vivekhere <vivekjai10@gmail.com>
2021-01-29 18:53:21 +0530

    switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -15,10 +15,12 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
@@ -27,8 +29,16 @@ class App extends Component {
 
 export default App;
 
-// Route
-// In the content area we want to render the component based on what we have
-// in the current url. That's when we use the Route component
-// We need to pass 2 props in the Route component as attributes
-// - path -> url and component -> component to be rendered based on the url.
+// The matching algorithm that the Route component uses checks to see if the current
+// url starts with the path specified if so then the component passed will be rendered.
+// That means if we go to /products/something the component will still be rendered.
+// Since, the path for home is "/" and the path for other components starts with "/"
+// the Home component will also be rendered when we go to other components.
+
+// To solve this probem
+// One solution is to add exact in the Home Route.
+// The other solution is to use the Switch component.
+// On the top import Switch component and wrap all the routes with a switch.
+// The Switch will render the first child that matches the location.
+// With switch we no longer need 'exact'.
+// We should order our routes from the most specific one's to the most generic one's.
