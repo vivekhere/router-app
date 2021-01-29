22f7e93fb3adf80508cd8901b54d0c8caab3b7e2
vivekhere <vivekjai10@gmail.com>
2021-01-29 18:39:33 +0530

    -- Routing -- adding routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -13,9 +14,21 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Route path="/products" component={Products} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/" component={Home} />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+// Route
+// In the content area we want to render the component based on what we have
+// in the current url. That's when we use the Route component
+// We need to pass 2 props in the Route component as attributes
+// - path -> url and component -> component to be rendered based on the url.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
+
+// Unlike frameworks like Angular, in React we do not have the concept of routing
+// because react is a simple light weight library and not a complete framework.
+// It is only responsible for rendering the view nothing more.
+
+// So, to add routing to our application we need to install a library called
+// react-router-dom -> npm i react-router-dom
+
+// BrowserRouter
+// On the top import BrowserRouter
+// This component wraps the history object in browsers and passes it down to
+// component tree. So anywhere in our component tree we can use the history object.
+
+// Next we want to register the routes that basically means we tell react what
+// component to render based on the given url. For that go to App.js
